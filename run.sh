2ee05e9cb527a2385244eb170d0f1022c93315b8
warren <chenweilunster@gmail.com>
2024-03-03 10:36:35 -0800

    fix style and SMART

--- run.sh
+++ run.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
 # All Pretrain
-python multitask_classifier.py --option pretrain --lr 1e-3 --use_gpu --tasks sst
-python multitask_classifier.py --option pretrain --lr 1e-3 --use_gpu --tasks quora
-python multitask_classifier.py --option pretrain --lr 1e-3 --use_gpu --tasks sts
-python multitask_classifier.py --option pretrain --lr 1e-3 --use_gpu --tasks cfimdb
+# python multitask_classifier.py --option pretrain --lr 1e-3 --use_gpu --tasks sst
+# python multitask_classifier.py --option pretrain --lr 1e-3 --use_gpu --tasks quora
+# python multitask_classifier.py --option pretrain --lr 1e-3 --use_gpu --tasks sts
+# python multitask_classifier.py --option pretrain --lr 1e-3 --use_gpu --tasks cfimdb
 
-# All Pretrain with SMART
-python multitask_classifier.py --option pretrain --lr 1e-3 --use_gpu --tasks sst --smart
+# # All Pretrain with SMART
+# python multitask_classifier.py --option pretrain --lr 1e-3 --use_gpu --tasks sst --smart
 python multitask_classifier.py --option pretrain --lr 1e-3 --use_gpu --tasks quora --smart
-python multitask_classifier.py --option pretrain --lr 1e-3 --use_gpu --tasks sts --smart
-python multitask_classifier.py --option pretrain --lr 1e-3 --use_gpu --tasks cfimdb --smart
+# python multitask_classifier.py --option pretrain --lr 1e-3 --use_gpu --tasks sts --smart
+# python multitask_classifier.py --option pretrain --lr 1e-3 --use_gpu --tasks cfimdb --smart
 
 # All finetune
-python multitask_classifier.py --option finetune --lr 1e-5 --use_gpu --tasks sst
-python multitask_classifier.py --option finetune --lr 1e-5 --use_gpu --tasks quora
-python multitask_classifier.py --option finetune --lr 1e-5 --use_gpu --tasks sts
-python multitask_classifier.py --option finetune --lr 1e-5 --use_gpu --tasks cfimdb
+# python multitask_classifier.py --option finetune --lr 1e-5 --use_gpu --tasks sst
+# python multitask_classifier.py --option finetune --lr 1e-5 --use_gpu --tasks quora
+# python multitask_classifier.py --option finetune --lr 1e-5 --use_gpu --tasks sts
+# python multitask_classifier.py --option finetune --lr 1e-5 --use_gpu --tasks cfimdb
 
-# All finetune with SMART
-python multitask_classifier.py --option finetune --lr 1e-5 --use_gpu --tasks sst --smart 
+# # All finetune with SMART
+# python multitask_classifier.py --option finetune --lr 1e-5 --use_gpu --tasks sst --smart 
 python multitask_classifier.py --option finetune --lr 1e-5 --use_gpu --tasks quora --smart 
-python multitask_classifier.py --option finetune --lr 1e-5 --use_gpu --tasks sts --smart 
-python multitask_classifier.py --option finetune --lr 1e-5 --use_gpu --tasks cfimdb --smart 
+# python multitask_classifier.py --option finetune --lr 1e-5 --use_gpu --tasks sts --smart 
+# python multitask_classifier.py --option finetune --lr 1e-5 --use_gpu --tasks cfimdb --smart 
 
-# Multitask pretrain
-python multitask_classifier.py --option pretrain --lr 1e-3 --use_gpu --tasks sst quora sts
-# (Just classification task)
-python multitask_classifier.py --option pretrain --lr 1e-3 --use_gpu --tasks sst quora
-# (Just pairwise task)
-python multitask_classifier.py --option pretrain --lr 1e-3 --use_gpu --tasks quora sts
+# # Multitask pretrain
+# python multitask_classifier.py --option pretrain --lr 1e-3 --use_gpu --tasks sst quora sts
+# # (Just classification task)
+# python multitask_classifier.py --option pretrain --lr 1e-3 --use_gpu --tasks sst quora
+# # (Just pairwise task)
+# python multitask_classifier.py --option pretrain --lr 1e-3 --use_gpu --tasks quora sts
 
 
 # Multitask pretrain with SMART
